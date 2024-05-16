30843b56e66db1c0b4f1d76df9add917a65e61c6
bervalim <bernardogvalim@gmail.com>
2024-05-16 18:03:41 -0300

    feat/user service methods

--- desafio_ng_ingressos_frontend/src/app/services/user.service.ts
+++ desafio_ng_ingressos_frontend/src/app/services/user.service.ts
@@ -1,9 +1,41 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import {
+  TLoginUserRequest,
+  TRegisterUserRequest,
+  TRegisterUserResponse,
+} from '../interfaces/user.interface';
+import { UserRequest } from '../api/user.request';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  readonly userSignal = signal<TRegisterUserResponse | null>(null);
 
-  constructor() { }
+  constructor(private userRequest: UserRequest) {
+    this.userRequest.userAutoLoginRequest()?.subscribe((data) => {
+      this.userSignal.set(data);
+    });
+  }
+
+  getUser() {
+    return this.userSignal();
+  }
+
+  registerUsersService(formData: TRegisterUserRequest) {
+    this.userRequest
+      .registerUsersRequest(formData)
+      .subscribe((data: TRegisterUserResponse) => {
+        console.log(data);
+        alert('Cadastro realizado com sucesso');
+      });
+  }
+
+  loginUsersService(formData: TLoginUserRequest) {
+    this.userRequest.loginUserRequest(formData).subscribe((data) => {
+      this.userSignal.set(data.user);
+      localStorage.setItem('@TokenNG', JSON.stringify(data.token));
+      localStorage.setItem('UserIdNG', JSON.stringify(data.user.id));
+    });
+  }
 }
