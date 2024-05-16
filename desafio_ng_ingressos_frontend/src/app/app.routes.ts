eed0becea675111dc2500b900f37c1f249104c4d
bervalim <bernardogvalim@gmail.com>
2024-05-16 12:26:55 -0300

    feat/create routes path on app.route

--- desafio_ng_ingressos_frontend/src/app/app.routes.ts
+++ desafio_ng_ingressos_frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  { path: 'dashboard', component: DashboardPageComponent },
+];
