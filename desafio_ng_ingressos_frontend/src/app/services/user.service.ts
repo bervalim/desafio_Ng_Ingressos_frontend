c6011036fa0ab81fff1cd94abf6a91ad04cf7132
bervalim <bernardogvalim@gmail.com>
2024-05-16 18:11:52 -0300

    feat/using navigateByUrl on user logic

--- desafio_ng_ingressos_frontend/src/app/services/user.service.ts
+++ desafio_ng_ingressos_frontend/src/app/services/user.service.ts
@@ -5,6 +5,7 @@ import {
   TRegisterUserResponse,
 } from '../interfaces/user.interface';
 import { UserRequest } from '../api/user.request';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,7 @@ import { UserRequest } from '../api/user.request';
 export class UserService {
   readonly userSignal = signal<TRegisterUserResponse | null>(null);
 
-  constructor(private userRequest: UserRequest) {
+  constructor(private userRequest: UserRequest, private router: Router) {
     this.userRequest.userAutoLoginRequest()?.subscribe((data) => {
       this.userSignal.set(data);
     });
@@ -28,6 +29,7 @@ export class UserService {
       .subscribe((data: TRegisterUserResponse) => {
         console.log(data);
         alert('Cadastro realizado com sucesso');
+        this.router.navigateByUrl('/');
       });
   }
 
@@ -36,6 +38,7 @@ export class UserService {
       this.userSignal.set(data.user);
       localStorage.setItem('@TokenNG', JSON.stringify(data.token));
       localStorage.setItem('@UserIdNG', JSON.stringify(data.user.id));
+      this.router.navigateByUrl('/dashboard');
     });
   }
 
@@ -43,5 +46,6 @@ export class UserService {
     this.userSignal.set(null);
     localStorage.removeItem('@TokenNG');
     localStorage.removeItem('@UserIdNG');
+    this.router.navigateByUrl('/');
   }
 }
