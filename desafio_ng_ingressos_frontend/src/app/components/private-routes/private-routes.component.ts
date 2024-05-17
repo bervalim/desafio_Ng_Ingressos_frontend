32680da8b6c38c49521cafb342a106641a618ca5
bervalim <bernardogvalim@gmail.com>
2024-05-16 22:30:27 -0300

    feat/private route protection improved

--- desafio_ng_ingressos_frontend/src/app/components/private-routes/private-routes.component.ts
+++ desafio_ng_ingressos_frontend/src/app/components/private-routes/private-routes.component.ts
@@ -4,20 +4,18 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-dashboard-page',
+  selector: 'app-private-routes',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './dashboard-page.component.html',
-  styleUrl: './dashboard-page.component.scss',
+  templateUrl: './private-routes.component.html',
+  styleUrl: './private-routes.component.scss',
 })
-export class DashboardPageComponent {
-  constructor(private userService: UserService) {}
+export class PrivateRoutesComponent {
+  constructor(private userService: UserService, private router: Router) {
+    if (!this.user) this.router.navigateByUrl('/');
+  }
 
   get user() {
     return this.userService.getUser();
-  }
-
-  handleLogout() {
-    this.userService.logoutUsersService();
   }
 }
