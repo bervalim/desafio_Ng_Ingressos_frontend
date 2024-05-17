2a2dfec7f46e9142a66b56f802d227c8f46f37a7
bervalim <bernardogvalim@gmail.com>
2024-05-16 23:58:48 -0300

    feat/create post request

--- desafio_ng_ingressos_frontend/src/app/api/post.request.ts
+++ desafio_ng_ingressos_frontend/src/app/api/post.request.ts
@@ -1,8 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { TCreatePostRequest } from '../interfaces/post.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostRequest {
-  constructor() {}
+  private BASE_URL = 'http://localhost:3333';
+
+  constructor(private http: HttpClient) {}
+
+  createPostRequest(formData: TCreatePostRequest) {
+    const token = localStorage.getItem('@TokenNG');
+
+    if (token) {
+      const parsedToken = JSON.parse(token);
+      return this.http.post(`${this.BASE_URL}/posts`, formData, {
+        headers: {
+          Authorization: `Bearer ${parsedToken}`,
+        },
+      });
+    } else {
+      return null;
+    }
+  }
 }
